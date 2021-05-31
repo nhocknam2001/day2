e9aad431d298ef5280855d33db0aa6751e589ef5
nhocknam2001 <77433450+nhocknam2001@users.noreply.github.com>
2021-05-31 16:33:09 +0700

    ok

--- wbe.js
+++ wbe.js
@@ -1,15 +1,16 @@
 const express = require('express')
 
-const app = express()
+const app= express()
 
 app.get('/index',(req,res)=>{
-  res.end('Hello world');  
+    res.sendFile(__dirname + '/public/index.html');
 })
 
 app.get('/aboutpage',(req,res)=>{
-    res.sendFile(__dirname + '/public/about.html');
+    res.sendFile(__dirname + '/public/aboutpage.html');
 })
 
-const PORT = 5000;
+
+const PORT =5000;
 app.listen(process.env.PORT || PORT);
 console.log('Server is running')
